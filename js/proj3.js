e81f9af2a856e41b97cdf49c2f75c6f92b57906d
Austine-hub <ajanjaaustine@gmail.com>
2025-07-10 18:28:56 +0300

    DID MULTIPLE EDTING

---
+++ js/proj3.js
@@ -0,0 +1,4 @@
+  function changeShot(imgName) {
+      document.getElementById('main-shot').src = imgName;
+      document.getElementById('shot-description').textContent = imgName.split('.')[0].replace('-', ' ');
+    }
